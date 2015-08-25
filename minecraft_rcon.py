556bf2e07201c6e1d2060e8cb23fbc1ad6f7df91
Sean LaPlante <laplantes@redmine.ais>
2015-08-24 22:27:46 -0400

    Minor fixes and updates

--- minecraft_rcon.py
+++ minecraft_rcon.py
@@ -13,13 +13,18 @@ import argparse
 import getpass
 import sys
 import os
+import platform
 from mcrcon import MCRcon
 
 def main():
+    if platform.system() == "Windows":
+        clear_str = 'cls'
+    else:
+        clear_str = 'clear'
+
     parser = argparse.ArgumentParser(description="Send commands to a remote Minecraft console")
-    parser.add_argument('-i','--ip',help="IP address of the server",required=True)
+    parser.add_argument('-h','--ip',help="IP address of the server",required=True)
     parser.add_argument('-p','--port',help="Port for RCON on the server",required=True)
-    parser.add_argument('-P','--password',help="Password for RCON on the server")
 
     args = parser.parse_args()
 
@@ -32,16 +37,20 @@ def main():
     print("Type 'exit' or 'quit' or cntrl-c to exit")
 
     while 1:
-        cmd = input('Remote Console>> ')
-        if cmd == 'cls' or cmd == 'clear':
-            os.system('cls')
-        elif cmd == 'exit' or cmd == 'quit':
-            rcon.close()
-            sys.exit(0)
-        elif cmd == "":
-            pass
-        else:
-            print(rcon.send(cmd))
+        try:
+            cmd = str(input('Remote Console>> '))
+            if cmd.strip().lower() in ['cls', 'clear']:
+                os.system(clear_str)
+            elif cmd.strip().lower() in ['exit', 'quit']:
+                rcon.close()
+                exit(0)
+            elif cmd.strip() == "":
+                pass
+            else:
+                print(rcon.send(cmd))
+        except EOFError:
+            print("ctrl-d detected, exiting...")
+            exit(0)
 
 if __name__=="__main__":
     main()
